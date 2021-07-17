604e815a7455b90ad83cec9d9140115a564dde8d
Trainee Ninja <ninjamaster9lives@gmail.com>
2021-07-17 12:35:12 +0300

    Refactored code Quotes_Scraper.py

--- Quotes_Scraper.py
+++ Quotes_Scraper.py
@@ -1,18 +1,39 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup as soup
 
-url = "http://quotes.toscrape.com/"
+filename= "quotes.csv"
+f= open(filename, "w")
+headers = "Quotes,Author\n"
+f.write(headers)
 
-def get_soup(url):
+URL = "http://quotes.toscrape.com/"
 
-	page = requests.get(url)
-	page_soup= soup(page.text, "html.parser")
+def connection(): #form a connection
+	with requests.Session() as session:
+		return session.get(URL)
 
-	text_array= page_soup.findAll("div", {"class":"quote"})
-	for container in text_array:
-		text_container = container.find("span", {"class":"text"})
-		text = text_container.get_text()
-		print(text+"\n")
+def soupa():  #form a soup of the page
+	return soup(connection().text, "html.parser")
+	
 
+def text_boxes(): #find out the text boxes in the page
+	text_boxes= soupa().findAll("div", {"class": "quote"})
+	return text_boxes
 
-get_soup(url)
+def quotes(a): #return quotes from the selected div
+	quotes= a.find("span", {"class" : "text"})
+	return quotes.get_text()
+
+def author(b): #return the author from the selected div
+	author = b.find("small", {"class" : "author"})
+	return author.get_text()
+
+def text():
+	q=1
+	for i in text_boxes():
+		print(str(q)+". " + quotes(i) + author(i))
+		f.write(quotes(i)+","+author(i)+"\n")
+		q+=1
+
+text()
+f.close()	
